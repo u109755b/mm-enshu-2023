a5a866d9c361b0855c298d0bb2d2fa60a4feb89b
breroom1234 <kikuchiryousuke1203@gmail.com>
2023-12-17 15:48:37 +0000

    specified to use current OpenAI API

--- image_gen/get_image.py
+++ image_gen/get_image.py
@@ -3,6 +3,8 @@
 import os
 import openai
 import csv
+import requests
+from openai import OpenAI
 from datetime import datetime
 
 def load_api_key(file_path):
@@ -19,16 +21,20 @@ def send_dalle_request(api_key, prompt, image_quality):
     response = openai.Image.create(prompt=prompt, n=1, size=image_quality)
     return response
 
-def save_image(image_id, novel_name, character_name):
-    """画像データをファイルに保存する"""
-    save_directory = f"../images/{novel_name}"
-    os.makedirs(save_directory, exist_ok=True)
-    save_path = os.path.join(save_directory, f"{character_name}.png")
+def save_image(image_url, save_path):
+    """画像データを指定されたパスに保存する"""
+    # 保存先ディレクトリの作成
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
 
-    with open(save_path, 'wb') as image_file:
-        image_file.write(openai.Image.download(image_id=image_id).content)
+    # 画像データの取得
+    response = requests.get(image_url)
+    if response.status_code == 200:
+        with open(save_path, 'wb') as image_file:
+            image_file.write(response.content)
+        return save_path
+    else:
+        raise Exception(f"Failed to download image: {response.status_code}")
 
-    return save_path
 
 def log_action(log_file_path, log_entry):
     """操作をCSVファイルにログとして記録する"""
@@ -40,9 +46,16 @@ def log_action(log_file_path, log_entry):
 
 def fetch_and_save_dalle_image_with_logging(novel_name, character_name, api_key_file_path, image_quality, log_file_path):
     """メイン関数：DALL-E APIを使用して画像を取得し、保存し、ログを記録する"""
-    api_key = load_api_key(api_key_file_path)
+    # api_key = load_api_key(api_key_file_path)
+    client = OpenAI(api_key_file_path)
     prompt = f"{novel_name}: {character_name}"
-    response = send_dalle_request(api_key, prompt, image_quality)
+    response = client.images.generate(
+        model='dall-e-3',
+        prompt=prompt,
+        size='1024x1024',
+        quality='standard',
+        n=1
+    )
 
     log_entry = {
         'timestamp': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
@@ -68,11 +81,11 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description='Fetch and save an image from DALL-E API.')
-    parser.add_argument('novel_name', type=str, help='The name of the novel.', default='My Novel')
-    parser.add_argument('character_name', type=str, help='The name of the character.', default='Main Character')
-    parser.add_argument('api_key_file_path', type=str, help='The path to the API key file.', default='./api_key.txt')
-    parser.add_argument('image_quality', type=str, choices=['high', 'middle', 'low'], help='The desired image quality.', default='middle')
-    parser.add_argument('log_file_path', type=str, help='The path to the log file.', default='./log.csv')
+    parser.add_argument('--novel_name', type=str, help='The name of the novel.', default='My Novel')
+    parser.add_argument('--character_name', type=str, help='The name of the character.', default='Main Character')
+    parser.add_argument('--api_key_file_path', type=str, help='The path to the API key file.', default='./api_key.txt')
+    parser.add_argument('--image_quality', type=str, choices=['high', 'middle', 'low'], help='The desired image quality.', default='middle')
+    parser.add_argument('--log_file_path', type=str, help='The path to the log file.', default='./log.csv')
 
     args = parser.parse_args()
 
