a7c479584e4b2da6a175a3e9159ec8ecf1e5b6ed
u109755b <u109755b@gmail.com>
2023-12-26 15:44:15 +0900

    Delete unused files

--- visualizer/urls_ver1.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('prev/', views.prev_paragraph, name='prev'),
-    path('next/', views.next_paragraph, name='next'),
-]
